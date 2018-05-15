440e4e09c44d6f618ba4006f246b82c6b48fd407
edsonjmv <edsonjmv@gmail.com>
2018-05-15 16:59:38 +0200

    Previous to npm run eject

--- src/App.js
+++ src/App.js
@@ -45,11 +45,13 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: '#2ecc71',
       font: 'inherit',
-      border: '1px solid blue',
+      color: '#000',
+      border: '0',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      outline: 'none',
     };
 
     let persons = null;
@@ -67,12 +69,22 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = '#e74c3c';
+      style.color = '#fff';
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>
