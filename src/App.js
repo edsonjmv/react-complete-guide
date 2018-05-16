090d5e720c86bbfb5cb17f5dd3a7d3b500516c74
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2018-05-16 13:06:17 +0200

    Adding ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -28,7 +29,7 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState( {persons: persons});
+    this.setState({persons: persons});
   }
 
   deletePersonHandler = (personIndex) => {
@@ -51,12 +52,13 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
+            return <ErrorBoundary key={person.id}>
+                     <Person
+                       click={() => this.deletePersonHandler(index)}
+                       name={person.name}
+                       age={person.age}
+                       changed={(event) => this.nameChangedHandler(event, person.id)} />
+                    </ErrorBoundary>
           })}
         </div>
       );
