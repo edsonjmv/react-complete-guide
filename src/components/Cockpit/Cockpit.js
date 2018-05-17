21ef4e9553e10ddfd9242cd3d723072512ef3bdc
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2018-05-17 16:24:53 +0200

    Adding props to main container

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -15,7 +15,7 @@ const cockpit = (props) => {
   }
   return (
     <div className={classes.Cockpit}>
-      <h1>Hi, I'm a React App</h1>
+      <h1>{props.appTitle}</h1>
       <p className={assignClasess.join(' ')}>This is really working!</p>
       <button 
         className={btnClass}
